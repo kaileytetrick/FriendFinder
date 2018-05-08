f6b8409416c6e69e769abb18e5c0c0214e283012
Kailey Tetrick <kaileytetrick88@gmail.com>
2018-05-08 01:52:12 -0600

    need friends api list to translate

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,30 +12,5 @@ module.exports = function (app) {
   
 };
 
-var userResponses = userInput.scores;
-
-		var matchName = '';
-		var matchImage = '';
-		var totalDifference = 1000; 
-
-	
-		for (var i = 0; i < friends.length; i++) {
-			var diff = 0;
-			for (var j = 0; j < userResponses.length; j++) {
-				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
-			}
-
-			if (diff < totalDifference) {
-			
-				totalDifference = diff;
-				matchName = friends[i].name;
-				matchImage = friends[i].photo;
-			}
-		}
-
-
-		friends.push(userInput);
-
-		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
 
 
